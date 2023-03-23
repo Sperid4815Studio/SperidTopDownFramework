3f9444dd28782a5610de4e5f3f8ea9e034be21af
k-terai <23315372+k-terai@users.noreply.github.com>
2023-03-24 03:54:57 +0900

    Add unity project

---
+++ Runtime/Scripts/RuntimeExample.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SperidTopDownFramework.Runtime
+{
+    public class RuntimeExample : MonoBehaviour
+    {
+        /// <summary>
+        /// tart is called before the first frame update
+        /// </summary>
+        void Start()
+        {
+
+        }
+
+        /// <summary>
+        /// Update is called once per frame
+        /// </summary>
+        void Update()
+        {
+
+        }
+    }
+
+}
