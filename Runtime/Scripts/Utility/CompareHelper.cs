7ded69003e39ed77c2e586772e14406c9535157f
k-terai <23315372+k-terai@users.noreply.github.com>
2023-07-01 21:33:54 +0900

    Add CompareHelper

---
+++ Runtime/Scripts/Utility/CompareHelper.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SperidTopDownFramework.Runtime
+{
+    public static class CompareHelper<T> where T : IComparable<T>
+    {
+        public static bool IsGreaterThanOrEqual(T value1, T value2)
+        {
+            return value1.CompareTo(value2) >= 0;
+        }
+
+        public static bool IsLessThanOrEqual(T value1, T value2)
+        {
+            return value1.CompareTo(value2) <= 0;
+        }
+
+        public static bool IsGreaterEqualAndLessEqual(T value1,T value2, T value3)
+        {
+            return IsGreaterThanOrEqual(value3, value1) && IsLessThanOrEqual(value3, value2);
+        }
+    }
+
+}
--- Runtime/Scripts/Utility/RandomPointInBoxCollider.cs
+++ Runtime/Scripts/Utility/RandomPointInBoxCollider.cs
@@ -4,30 +4,13 @@ using UnityEngine;
 
 namespace SperidTopDownFramework.Runtime
 {
-    public class RandomPointInBoxCollider : MonoBehaviour
+    public static class RandomPointInBoxCollider
     {
-        [SerializeField]
-        private BoxCollider _boxCollider;
-
-        private void Start()
-        {
-            if (_boxCollider != null)
-            {
-                _boxCollider = GetComponent<BoxCollider>();
-            }
-
-            if (_boxCollider == null)
-            {
-                Debug.LogError("BoxCollider is not assigned!");
-                return;
-            }
-        }
-
-        public Vector3 GetRandomPointInBoxCollider()
+        public static Vector3 GetRandomPointInBoxCollider(BoxCollider collider)
         {
             Vector3 result = Vector3.zero;
-            Vector3 boundsMin = _boxCollider.bounds.min;
-            Vector3 boundsMax = _boxCollider.bounds.max;
+            Vector3 boundsMin = collider.bounds.min;
+            Vector3 boundsMax = collider.bounds.max;
 
             float x = Random.Range(boundsMin.x, boundsMax.x);
             float y = Random.Range(boundsMin.y, boundsMax.y);
